e69ed0230d458b7312799aea96bf3aa37d1b2e05
Thiccy <birhic023@gmail.com>
2022-01-03 21:09:16 +0300

    Renewed 1.3.1
    * Delete snap.html
    
    * Update package.json
    
    * Delete lib/paste directory
    
    * Delete lib/download directory
    
    * Delete ephoto.js
    
    * Update textpro.js
    
    * Update photooxy.js
    
    * Update index.js
    
    * Update README.md

--- src/index.js
+++ src/index.js
@@ -1,7 +1,2 @@
 exports.textpro = require("../lib/textmaker/textpro");
 exports.photooxy = require("../lib/textmaker/photooxy");
-exports.ephoto = require("../lib/textmaker/ephoto");
-exports.pastegg = require("../lib/paste/pastegg");
-exports.keeptiktok = require("../lib/download/keeptiktok");
-exports.snaptik = require("../lib/download/snaptik");
-exports.instagram = require("../lib/download/instagram");
