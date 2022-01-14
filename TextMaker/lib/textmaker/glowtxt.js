f756b1e130f859f917d7952ce144637c9fa4cfe2
Thiccy <birhic023@gmail.com>
2022-01-15 02:44:11 +0300

    Update glowtxt.js

--- TextMaker/lib/textmaker/glowtxt.js
+++ TextMaker/lib/textmaker/glowtxt.js
@@ -243,32 +243,48 @@ const axios = require("axios");
      if (typeof options.non_trans == "boolean") {
          options.non_trans = options.non_trans.toString()
      }
-     var data = await axios.get(
-         `https://glowtxt.com/gentext2.php?text=${options.text}&text2=${options.text2}&text3=${options.text3}&font_style=${options.font_style}&font_size=${options.font_size}&font_colour=${options.font_colour}&bgcolour=${options.bgcolour}&glow_halo=${options.glow_halo}&non_trans=${options.non_trans}&glitter_border=${options.glitter_border}&anim_type=${options.anim_type}&sumbit_type=${options.submit_type}`, {
-             headers: {
-                 accept: "*/*",
-                 referer: "https://glowtxt.com/",
-                 "sec-fetch-dest": "empty",
-                 "sec-fetch-mode": "cors",
-                 "sec-fetch-site": "same-origin",
-                 "accept-encoding": "gzip, deflate, br",
-                 "accept-language": "tr-TR,tr;q=0.9,en-US;q=0.8,en;q=0.7",
-                 "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36",
-                 cookie: "__utmc=73139909; _ga=GA1.2.760919237.1641592404; _gid=GA1.2.437793348.1641592406; vc=2; __utma=73139909.760919237.1641592404.1641598204.1641651676.3; __utmz=73139909.1641651676.3.2.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided); __utmt=1; __utmb=73139909.1.10.1641651676; _gat=1"
-             }
-         }
-     )
-     var schema = new Object();
-     try {
-         var trydata = data.data.toString().split("<fullfilename>")[1].split("<")[0]
-         schema.status = true;
-     } catch (err) {
-         schema.status = false;
-         schema.error = err.toString();
-     }
-     if (schema.status) {
-         schema.image = "https://static1.glowtxt.com/" + data.data.toString().split("<datadir>")[1].split("<")[0] + "/" + data.data.toString().split("<fullfilename>")[1].split("<")[0]
+     var non_english;
+     var regex = /[^\x00-\x7F]+/g
+     if (
+         regex.test(options.text)  == true ||
+         regex.test(options.text2) == true || 
+         regex.test(options.text3) == true
+    ) {
+        non_english = true
+    } else {
+        non_english = false
+    }
+    var schema = new Object();
+     if (!non_english) {
+        var data = await axios.get(
+            `https://glowtxt.com/gentext2.php?text=${options.text}&text2=${options.text2}&text3=${options.text3}&font_style=${options.font_style}&font_size=${options.font_size}&font_colour=${options.font_colour}&bgcolour=${options.bgcolour}&glow_halo=${options.glow_halo}&non_trans=${options.non_trans}&glitter_border=${options.glitter_border}&anim_type=${options.anim_type}&sumbit_type=${options.submit_type}`, {
+                headers: {
+                    accept: "*/*",
+                    referer: "https://glowtxt.com/",
+                    "sec-fetch-dest": "empty",
+                    "sec-fetch-mode": "cors",
+                    "sec-fetch-site": "same-origin",
+                    "accept-encoding": "gzip, deflate, br",
+                    "accept-language": "tr-TR,tr;q=0.9,en-US;q=0.8,en;q=0.7",
+                    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36",
+                    cookie: "__utmc=73139909; _ga=GA1.2.760919237.1641592404; _gid=GA1.2.437793348.1641592406; vc=2; __utma=73139909.760919237.1641592404.1641598204.1641651676.3; __utmz=73139909.1641651676.3.2.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided); __utmt=1; __utmb=73139909.1.10.1641651676; _gat=1"
+                }
+            }
+        )
+        try {
+            var trydata = data.data.toString().split("<fullfilename>")[1].split("<")[0]
+            schema.status = true;
+        } catch (err) {
+            schema.status = false;
+            schema.error = err.toString();
+        }
+        if (schema.status) {
+            schema.image = "https://static1.glowtxt.com/" + data.data.toString().split("<datadir>")[1].split("<")[0] + "/" + data.data.toString().split("<fullfilename>")[1].split("<")[0]
+        }
+     } else {
+         schema.status = false
+         schema.error = "Please Use Only English Characters."
      }
      return schema;
- }
+}
 module.exports = glowtext;
